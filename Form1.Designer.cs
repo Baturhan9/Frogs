cadb80e8152be118e6e39bcfc518a3647e854a5f
ST3606 <ST3606@ST3606>
2023-04-25 11:36:24 +0600

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace Frogsssss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.RightFrog1 = new System.Windows.Forms.PictureBox();
+            this.RightFrog2 = new System.Windows.Forms.PictureBox();
+            this.RightFrog3 = new System.Windows.Forms.PictureBox();
+            this.LeftFrog1 = new System.Windows.Forms.PictureBox();
+            this.LeftFrog2 = new System.Windows.Forms.PictureBox();
+            this.LeftFrog3 = new System.Windows.Forms.PictureBox();
+            this.BtnNextMove = new System.Windows.Forms.Button();
+            this.pictureVoid = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RightFrog1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RightFrog2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RightFrog3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LeftFrog1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LeftFrog2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LeftFrog3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureVoid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(10, 179);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(119, 94);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(135, 179);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(119, 94);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(260, 179);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(119, 94);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(385, 179);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(119, 94);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(510, 179);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(119, 94);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 0;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(635, 179);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(119, 94);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 0;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+            this.pictureBox7.Location = new System.Drawing.Point(760, 179);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(119, 94);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 0;
+            this.pictureBox7.TabStop = false;
+            // 
+            // RightFrog1
+            // 
+            this.RightFrog1.Image = ((System.Drawing.Image)(resources.GetObject("RightFrog1.Image")));
+            this.RightFrog1.Location = new System.Drawing.Point(20, 108);
+            this.RightFrog1.Name = "RightFrog1";
+            this.RightFrog1.Size = new System.Drawing.Size(96, 65);
+            this.RightFrog1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.RightFrog1.TabIndex = 1;
+            this.RightFrog1.TabStop = false;
+            this.RightFrog1.Click += new System.EventHandler(this.RightFrog1_Click);
+            // 
+            // RightFrog2
+            // 
+            this.RightFrog2.Image = ((System.Drawing.Image)(resources.GetObject("RightFrog2.Image")));
+            this.RightFrog2.Location = new System.Drawing.Point(149, 108);
+            this.RightFrog2.Name = "RightFrog2";
+            this.RightFrog2.Size = new System.Drawing.Size(96, 65);
+            this.RightFrog2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.RightFrog2.TabIndex = 1;
+            this.RightFrog2.TabStop = false;
+            this.RightFrog2.Click += new System.EventHandler(this.RightFrog2_Click);
+            // 
+            // RightFrog3
+            // 
+            this.RightFrog3.Image = ((System.Drawing.Image)(resources.GetObject("RightFrog3.Image")));
+            this.RightFrog3.Location = new System.Drawing.Point(273, 108);
+            this.RightFrog3.Name = "RightFrog3";
+            this.RightFrog3.Size = new System.Drawing.Size(96, 65);
+            this.RightFrog3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.RightFrog3.TabIndex = 1;
+            this.RightFrog3.TabStop = false;
+            this.RightFrog3.Click += new System.EventHandler(this.RightFrog3_Click);
+            // 
+            // LeftFrog1
+            // 
+            this.LeftFrog1.Image = ((System.Drawing.Image)(resources.GetObject("LeftFrog1.Image")));
+            this.LeftFrog1.Location = new System.Drawing.Point(510, 113);
+            this.LeftFrog1.Name = "LeftFrog1";
+            this.LeftFrog1.Size = new System.Drawing.Size(100, 60);
+            this.LeftFrog1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.LeftFrog1.TabIndex = 2;
+            this.LeftFrog1.TabStop = false;
+            this.LeftFrog1.Click += new System.EventHandler(this.LeftFrog1_Click);
+            // 
+            // LeftFrog2
+            // 
+            this.LeftFrog2.Image = ((System.Drawing.Image)(resources.GetObject("LeftFrog2.Image")));
+            this.LeftFrog2.Location = new System.Drawing.Point(644, 113);
+            this.LeftFrog2.Name = "LeftFrog2";
+            this.LeftFrog2.Size = new System.Drawing.Size(100, 60);
+            this.LeftFrog2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.LeftFrog2.TabIndex = 2;
+            this.LeftFrog2.TabStop = false;
+            this.LeftFrog2.Click += new System.EventHandler(this.LeftFrog2_Click);
+            // 
+            // LeftFrog3
+            // 
+            this.LeftFrog3.Image = ((System.Drawing.Image)(resources.GetObject("LeftFrog3.Image")));
+            this.LeftFrog3.Location = new System.Drawing.Point(769, 113);
+            this.LeftFrog3.Name = "LeftFrog3";
+            this.LeftFrog3.Size = new System.Drawing.Size(100, 60);
+            this.LeftFrog3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.LeftFrog3.TabIndex = 2;
+            this.LeftFrog3.TabStop = false;
+            this.LeftFrog3.Click += new System.EventHandler(this.LeftFrog3_Click);
+            // 
+            // BtnNextMove
+            // 
+            this.BtnNextMove.Location = new System.Drawing.Point(385, 353);
+            this.BtnNextMove.Name = "BtnNextMove";
+            this.BtnNextMove.Size = new System.Drawing.Size(119, 48);
+            this.BtnNextMove.TabIndex = 4;
+            this.BtnNextMove.Text = "ХОД";
+            this.BtnNextMove.UseVisualStyleBackColor = true;
+            this.BtnNextMove.Click += new System.EventHandler(this.BtnNextMove_Click);
+            // 
+            // pictureVoid
+            // 
+            this.pictureVoid.Location = new System.Drawing.Point(388, 123);
+            this.pictureVoid.Name = "pictureVoid";
+            this.pictureVoid.Size = new System.Drawing.Size(100, 50);
+            this.pictureVoid.TabIndex = 5;
+            this.pictureVoid.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(782, 406);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 32);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Restart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureVoid);
+            this.Controls.Add(this.BtnNextMove);
+            this.Controls.Add(this.LeftFrog3);
+            this.Controls.Add(this.LeftFrog2);
+            this.Controls.Add(this.LeftFrog1);
+            this.Controls.Add(this.RightFrog3);
+            this.Controls.Add(this.RightFrog2);
+            this.Controls.Add(this.RightFrog1);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RightFrog1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RightFrog2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RightFrog3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LeftFrog1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LeftFrog2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LeftFrog3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureVoid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox RightFrog1;
+        private System.Windows.Forms.PictureBox RightFrog2;
+        private System.Windows.Forms.PictureBox RightFrog3;
+        private System.Windows.Forms.PictureBox LeftFrog1;
+        private System.Windows.Forms.PictureBox LeftFrog2;
+        private System.Windows.Forms.PictureBox LeftFrog3;
+        private System.Windows.Forms.Button BtnNextMove;
+        private System.Windows.Forms.PictureBox pictureVoid;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using DevExpress.Utils.Design;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Frogsssss
+{
+    
+    public partial class Form1 : Form
+    {
+        bool isRight = false;
+        int indexMove = -1;
+
+        char[] map = { '*', '*','*','-','#','#','#' };
+        Point[] locationp = new Point[7];
+        PictureBox[] frogsPicture = new PictureBox[7];
+        public Form1()
+        {
+            InitializeComponent();
+            locationp[0] = RightFrog1.Location;
+            locationp[1] = RightFrog2.Location;
+            locationp[2] = RightFrog3.Location;
+            locationp[3] = pictureVoid.Location;
+            locationp[4] = LeftFrog1.Location;
+            locationp[5] = LeftFrog2.Location;
+            locationp[6] = LeftFrog3.Location;
+
+            frogsPicture[0] = RightFrog1;
+            frogsPicture[1] = RightFrog2;
+            frogsPicture[2] = RightFrog3;
+            frogsPicture[3] = pictureVoid;
+            frogsPicture[4] = LeftFrog1;
+            frogsPicture[5] = LeftFrog2;
+            frogsPicture[6] = LeftFrog3;
+
+        }
+
+        private bool CanJump (int index)
+        {
+            if (isRight && (map[index + 1] == '-' || map[index + 2] == '-'))
+                return true;
+            else if (!isRight && (map[index - 1] == '-' || map[index- 2] == '-'))
+                return true;
+            return false;
+        }
+
+        private int FindIndexFrogs(PictureBox pb)
+        {
+            for(int i = 0; i < frogsPicture.Length; i++)
+            {
+                if(frogsPicture[i] == pb)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private int FindIndexSpace()
+        {
+            for (int i = 0; i < frogsPicture.Length; i++)
+            {
+                if (frogsPicture[i] == pictureVoid)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private void Swamp (int indexfrogs, int indexspace)
+        {
+            PictureBox temp = frogsPicture[indexfrogs];
+            frogsPicture[indexfrogs] = frogsPicture[indexspace];
+            frogsPicture[indexspace] = temp;
+
+            char ctemp = map[indexfrogs];
+            map[indexfrogs] = map[indexspace];
+            map[indexspace] = ctemp;
+
+            frogsPicture[indexspace].Location = locationp[indexspace];
+            frogsPicture[indexfrogs].Location = locationp[indexfrogs];
+
+            
+        }
+
+        private void RightFrog1_Click(object sender, EventArgs e)
+        {
+            isRight = true;
+            indexMove = FindIndexFrogs(RightFrog1);
+        }
+
+        private void RightFrog2_Click(object sender, EventArgs e)
+        {
+            isRight = true;
+            indexMove = FindIndexFrogs(RightFrog2);
+        }
+
+        private void RightFrog3_Click(object sender, EventArgs e)
+        {
+            isRight= true;
+            indexMove = FindIndexFrogs(RightFrog3);
+        }
+
+        private void LeftFrog1_Click(object sender, EventArgs e)
+        {
+            isRight = false;
+            indexMove = FindIndexFrogs(LeftFrog1);
+        }
+
+        private void LeftFrog2_Click(object sender, EventArgs e)
+        {
+            isRight = false;
+            indexMove = FindIndexFrogs(LeftFrog2);
+        }
+
+        private void LeftFrog3_Click(object sender, EventArgs e)
+        {
+            isRight = false;
+            indexMove = FindIndexFrogs(LeftFrog3);
+        }
+
+        private void BtnNextMove_Click(object sender, EventArgs e)
+        {
+            if (indexMove != -1 && CanJump(indexMove))
+            {
+                Swamp(indexMove, FindIndexSpace());
+                indexMove = -1;
+            }
+            else
+            {
+                MessageBox.Show("Невозможно сделать ход");
+                return;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frogsPicture[0] = RightFrog1;
+            frogsPicture[1] = RightFrog2;
+            frogsPicture[2] = RightFrog3;
+            frogsPicture[3] = pictureVoid;
+            frogsPicture[4] = LeftFrog1;
+            frogsPicture[5] = LeftFrog2;
+            frogsPicture[6] = LeftFrog3;
+
+            for(int i = 0; i < frogsPicture.Length; i++)
+                frogsPicture[i].Location = locationp[i];
+        }
+    }
+}
